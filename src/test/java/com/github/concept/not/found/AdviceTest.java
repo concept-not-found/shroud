cf2b74db23cae7ad54118e15c263c9da4542872a
Pyrolistical <pyrogx1133@gmail.com>
2012-07-11 16:03:28 -0700

    added gitignore

--- src/test/java/com/github/concept/not/found/AdviceTest.java
+++ src/test/java/com/github/concept/not/found/AdviceTest.java
@@ -27,7 +27,7 @@ public class AdviceTest {
 	}
 
 	public class SoundEffects {
-		public String attack(final Object target, final Method method, final Object[] parameters) throws Exception {
+		public Object attack(final Object target, final Method method, final Object[] parameters) throws Exception {
 			return "pew pew " + method.invoke(target, parameters);
 		}
 	}
